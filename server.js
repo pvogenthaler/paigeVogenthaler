413a58f89bbb288b7cd54c2cc4f7abb7ff0f6952
Paige <paige.vogenthaler@gmail.com>
2017-11-03 23:02:34 -0500

    fix directory structure

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + '/app'));
+app.use(express.static(__dirname));
 
 app.listen(port, function() {
   console.log('Our app is running on http://localhost:' + port);
