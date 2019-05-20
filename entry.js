6f123122eca1fdbdb3d5620f726c6caa4844e989
Paige Vogenthaler <paige.vogenthaler@vividseats.com>
2019-05-19 23:20:30 -0500

    fix entry file

--- entry.js
+++
@@ -1,7 +0,0 @@
-if (NODE_ENV === 'development') {
-  require('../src/react/App');
-} else if (NODE_ENV === 'production') {
-  var script = document.createElement('script');
-  script.src = 'https://s3-us-west-1.amazonaws.com/pvogenthaler.github.io/bundles/bundle.js';
-  document.body.appendChild(script);
-}
