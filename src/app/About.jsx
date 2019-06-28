bc544cf2ae3e360a0af8bf30cde813b4dce994d6
Paige Vogenthaler <paige.vogenthaler@vividseats.com>
2019-06-27 22:42:40 -0500

    move CNAME

--- src/app/About.jsx
+++ src/app/About.jsx
@@ -2,21 +2,18 @@ import React from 'react';
 
 const About = () => (
     <div className="about">
-      <p>
-        I'm a software engineer specialized in JavaScript, ES6, and React. I lead
-        my team in developing clean, maintainable, reusable, testable, and scalable
-        production code for impactful technologies.
-      </p>
-      <p>
-        Languages: JavaScript/ ES6, Java<br/>
-        Web: HTML5, CSS3, Node JS, JSP, AJAX, REST<br/>
-        Frameworks/ Libraries: React JS, Redux, React Native, jQuery, MapBox, Express JS, SASS<br/>
-        Databases: SQL/ MySQL, NoSQL/ MongoDB<br/>
-        Testing: Mocha, Chai, Cucumber<br/>
-        Build Tools: Webpack, Babel, Gulp, Grunt, Maven<br/>
-        Workflow: Agile/ Scrum, Version Control/ Git, Jira<br/>
-        Methodologies: TDD, BDD, OOP<br/>
-      </p>
+        <h2>Paige Vogenthaler</h2>
+        <p>
+            I am an engineering leader responsible managing a UI team focused on
+            delivering A/B tests and new front end features. I also founded and lead
+            the Vivid Women in Tech organization, which provides leadership and
+            learning opportunities for women and promotes community and equality in
+            the workplace.
+        </p>
+        <p>
+            In my free time, I enjoy playing Beethoven and Chopin on the piano
+            and spending time with my loving, orange tabby cat, Howie.
+        </p>
     </div>
 );
 
