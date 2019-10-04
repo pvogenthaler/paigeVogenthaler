c057ee54be82ca40753d3f800ede15d5dddec255
Paige Vogenthaler <paige.vogenthaler@vividseats.com>
2019-10-03 23:58:51 -0500

    add 404 page

---
+++ public/styles-f1dcbc288744b58dbf33.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{209:function(n,w,o){}}]);
+//# sourceMappingURL=styles-f1dcbc288744b58dbf33.js.map
---
+++ src/pages/404.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+const Contact = () => (
+    <Layout>
+        <div className="404">
+            Sorry, this page doesn't exist. Head back to the <a href='/'>homepage</a>.
+        </div>
+    </Layout>
+);
+
+export default Contact;
